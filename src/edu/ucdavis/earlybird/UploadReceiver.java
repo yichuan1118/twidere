a0db3ae3f411c56755c44e396b9d2d7d67921703
Yichuan Wang <yichuan1118@gmail.com>
2012-10-30 14:19:21 +0800

    profiling wifi connection

--- src/edu/ucdavis/earlybird/UploadReceiver.java
+++ src/edu/ucdavis/earlybird/UploadReceiver.java
@@ -3,6 +3,7 @@ package edu.ucdavis.earlybird;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 
 public class UploadReceiver extends BroadcastReceiver {
 
@@ -12,6 +13,11 @@ public class UploadReceiver extends BroadcastReceiver {
 	public void onReceive(final Context context, final Intent intent) {
 		final boolean isWifi = ProfilingUtil.isOnWifi(context.getApplicationContext());
 		final boolean isCharging = ProfilingUtil.isCharging(context.getApplicationContext());
+		
+		if(intent.getAction().equals(WifiManager.SUPPLICANT_CONNECTION_CHANGE_ACTION)){
+			final boolean wifi = intent.getBooleanExtra(WifiManager.EXTRA_SUPPLICANT_CONNECTED, false);
+			ProfilingUtil.profiling(context, ProfilingUtil.FILE_NAME_WIFI, wifi?"connected":"disconnected");
+		}
 
 		if (isWifi && isCharging) {
 			new UploadTask(context).execute();
